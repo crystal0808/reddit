af00d45e7b8885af16e1657cc1cca58a29ae1c67
crystal\crystal <crystalcodeliu@gmail.com>
2018-11-27 23:21:37 -0800

    Added Random

--- src/App.js
+++ src/App.js
@@ -10,20 +10,66 @@ class App extends Component {
         super(props);
         this.state = {
             data: [],
+            subreddit: 'all',
         };
     }
 
     callback(key) {
         console.log(key);
-        let query = 'hot';
-        if (key == 2) {
-            query = 'new';
-        } else if (key == 3) {
-            query = 'top';
+        let sortBy = 'hot';
+        let subreddit = this.state.subreddit;
+
+        if (key == 4) {
+            //fetch new subreddit
+            let url = "https://www.reddit.com/r/all/random.json";
+            fetch(url).then(res => res.json()).then((result) => {
+                    console.log(result)
+                    subreddit = result[0].data.children[0].data.subreddit;
+                    console.log(subreddit)
+                    this.setState({
+                        subreddit: subreddit,
+                    })
+                }
+            )
+            url = "https://www.reddit.com/r/" + subreddit + "/" + sortBy + ".json";
+            console.log(url)
+            fetch(url).then(res => res.json()).then((result) => {
+                    console.log(result)
+                    this.setState({
+                        data: result.data.children,
+                    })
+                }
+            )
+        } else {
+            if (key == 3) {
+                sortBy = 'top';
+            } else if (key == 2) {
+                sortBy = 'new';
+            }
+            let url = "https://www.reddit.com/r/" + subreddit + "/" + sortBy + ".json";
+            console.log(url)
+            fetch(url).then(res => res.json()).then((result) => {
+                    console.log(result)
+                    this.setState({
+                        data: result.data.children,
+                    })
+                }
+            )
         }
-        let url = "https://www.reddit.com/r/subreddit/" + query + ".json";
+    }
+
+    callbackMenu(e) {
+        console.log(e.key);
+        let subreddit = 'all';
+        let url = "https://www.reddit.com/r/all/.json";
+        if (e.key == 2) {
+            subreddit = 'popular';
+            url = "https://www.reddit.com/r/popular.json";
+        }
+        this.setState({subreddit: subreddit});
+
         fetch(url).then(res => res.json()).then((result) => {
-            console.log(result.data.children)
+                console.log(result)
                 this.setState({
                     data: result.data.children,
                 })
@@ -39,7 +85,7 @@ class App extends Component {
             width: '7%',
             render: (data) => (
                 <span>
-                    <img color="blue" alt="" src={data.thumbnail} height= '70'
+                    <img color="blue" alt="" src={data.thumbnail} height='70'
                          width='70'></img>
                 </span>
             ),
@@ -51,38 +97,38 @@ class App extends Component {
             render: (data) => {
                 let link = "https://www.reddit.com/r/subreddit/comments/" + data.id;
                 return (<div>
-                    <a href={link}>{data.title}</a>
-                    <div>{data.num_comments} comments</div>
+                        <a href={link}>{data.title}</a>
+                        <div>{data.num_comments} comments</div>
                     </div>
                 )
             }
-        }, ];
+        },];
         return (
             <Layout className="layout">
-                <Header>
-                    <div className="logo"/>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                        style={{lineHeight: '64px'}}
-                    >
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
-                    </Menu>
-                </Header>
-                <Content >
+                <Menu
+                    theme="light"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{lineHeight: '64px'}}
+                    onClick={this.callbackMenu.bind(this)}
+                >
+                    <Menu.Item key="1">POPULAR</Menu.Item>
+                    <Menu.Item key="2">ALL</Menu.Item>
+                </Menu>
+                <Content>
                     <div style={{background: '#fff', padding: 24, minHeight: 280}}>
                         <Tabs defaultActiveKey="1" onChange={this.callback.bind(this)}>
                             <TabPane tab="Hot" key="1">
-                                <Table showHeader = {false} columns={columns} dataSource={this.state.data}/>
+                                <Table showHeader={false} columns={columns} dataSource={this.state.data}/>
                             </TabPane>
                             <TabPane tab="New" key="2">
-                                <Table showHeader = {false} columns={columns} dataSource={this.state.data}/>
+                                <Table showHeader={false} columns={columns} dataSource={this.state.data}/>
                             </TabPane>
                             <TabPane tab="Top" key="3">
-                                <Table showHeader = {false} columns={columns} dataSource={this.state.data}/>
+                                <Table showHeader={false} columns={columns} dataSource={this.state.data}/>
+                            </TabPane>
+                            <TabPane tab="Random" key="4">
+                                <Table showHeader={false} columns={columns} dataSource={this.state.data}/>
                             </TabPane>
                         </Tabs>
 
@@ -97,7 +143,7 @@ class App extends Component {
 
 
     componentDidMount() {
-        fetch("https://www.reddit.com/r/subreddit/new.json?sort=new").then(res => res.json()).then((result) => {
+        fetch("https://www.reddit.com/r/all/hot.json?sort=hot").then(res => res.json()).then((result) => {
                 this.setState({
                     data: result.data.children,
                 })
